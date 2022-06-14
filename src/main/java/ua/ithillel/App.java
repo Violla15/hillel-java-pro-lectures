6b93befdd8120c70c46781d8978b50b6b19dacbf
Yulia Kachan <yuleyka@ukr.net>
2022-06-14 20:05:49 +0300

    Added lectures and homeworks packages. Added Lecture2 and Homework2 classes

--- src/main/java/ua/ithillel/App.java
+++
@@ -1,9 +0,0 @@
-package ua.ithillel;
-
-public class App
-{
-    public static void main( String[] args )
-    {
-        System.out.println( "Hello World!" );
-    }
-}
---
+++ src/main/java/ua/ithillel/lectures/lecture2/Lecture2.java
@@ -0,0 +1,8 @@
+package ua.ithillel.lectures.lecture2;
+
+public class Lecture2 {
+
+    public static void main(String[] args) {
+
+    }
+}
