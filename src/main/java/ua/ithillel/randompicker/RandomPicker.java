44b3af1bb0726a52ec782ac43b94f84041081cf3
Yulia Kachan <yuleyka@ukr.net>
2022-06-21 18:56:10 +0300

    Added random picker

---
+++ src/main/java/ua/ithillel/randompicker/RandomPicker.java
@@ -0,0 +1,29 @@
+package ua.ithillel.randompicker;
+
+import java.util.List;
+import java.util.Random;
+
+public class RandomPicker {
+
+    public static void main(String[] args) {
+        List<String> students = List.of("Anna Pchelintseva",
+                "Krushniak Oleksandr",
+                "Valeriia Kosohova",
+                "Violetta Churikova",
+                "Yegor Artsev",
+                "Анастасія Лавнюженкова",
+                "Валерий Лабеев",
+                "Олександр Красножон",
+                "Петро Коротушак",
+                "Illia Karmazinskyi",
+                "Andrii Kontsevoda",
+                "Вячеслав Омельчук",
+                "Bogdan Tokarenko",
+                "Алексей Мушкамбарян",
+                "Володимир Семирозум",
+                "Valerii Dvoretskyi");
+
+        System.out.println(students.get(new Random().nextInt(students.size())));
+    }
+
+}
