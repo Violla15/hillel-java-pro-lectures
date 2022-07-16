91551b2803b5e872b02cf437a1ee01f78bdf38e0
Stepan <kachanstepan@gmail.com>
2022-07-16 11:21:30 +0300

    Lecture 11 code

---
+++ src/main/java/ua/ithillel/lectures/lecture11/Lecture11.java
@@ -0,0 +1,13 @@
+package ua.ithillel.lectures.lecture11;
+
+public class Lecture11 {
+
+    public static void main(String[] args) {
+        // 1) Illia Karmazinaskiy
+        // 2) Petro Korotushak
+        // 3) Andrey Koncevoda
+        // 4) Violetta
+        // 5) Bogdan Tokarenko
+        
+    }
+}
